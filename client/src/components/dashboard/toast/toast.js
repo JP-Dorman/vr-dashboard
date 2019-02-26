14ea6c637257f47cc685c3154fe2423fec662844
Joe Dorman <joe_parsons-dorman@shelter.org.uk>
2019-02-26 12:41:22 +0000

    Login test

---
+++ client/src/components/dashboard/toast/toast.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './toast.css'
+import * as firebase from 'firebase';
+
+
+class Modal extends React.Component {
+
+    render() {
+        const modalHeaderContent = this.props.modalData[0];
+        const modalBodyContent = this.props.modalData[1];
+        const showClass = this.props.modalShow === true ? 'show' : '';
+
+        return (
+            <div id="">
+
+            </div>
+        );
+    };
+}
+
+export default Modal;
